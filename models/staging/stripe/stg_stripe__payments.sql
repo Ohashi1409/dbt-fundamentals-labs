de7da73d41c8c2daec7f052614a433e7fdb3a6c0
João Ohashi <ohashiramos@gmail.com>
2026-04-30 18:11:47 +0000

    referenced sources in staging models

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,24 +1,10 @@
-with 
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    amount as payment_amount,
+    created as payment_created,
+    _batched_at
 
-source as (
-
-    select * from raw.stripe.payment   
-
-),
-
-renamed as (
-
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount as payment_amount,
-        created as payment_created,
-        _batched_at
-
-    from source
-
-)
-
-select * from renamed
+from {{ source('stripe', 'payment') }}
